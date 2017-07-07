ebedbc5696f50167dbdca7f1c5c779814b586d6d
jbwebb <jbwebb@bcm.edu>
2017-07-07 13:51:28 -0500

    fixed toolbox initialization and saving issues

--- python/learn.py
+++ python/learn.py
@@ -81,30 +81,32 @@ def initialize_vizdoom(config_file):
     print("Doom initialized.")
     return game
 
+# Initialize agent and TensorFlow graph
 game = initialize_vizdoom(config_file_path)
 sess = tf.Session()
 agent = Agent(game=game, agent_file=agent_file_path, action_set=action_set,
               session=sess)
-
-# Initialize variables
-time_start = time()
 init = tf.global_variables_initializer()
 sess.run(init)
 
 if trackable:
     np.savetxt(results_directory + "action_indices.txt", agent.action_indices)
 
+# Initialize toolbox
 layer_shapes = agent.get_layer_shape(layer_names)
-layer_sizes = [len(layer_shapes) for i in range(len(layer_shapes))]
-print(layer_sizes)
+layer_sizes = np.ones(len(layer_shapes), dtype=np.int64)
+for i in range(len(layer_shapes)):
+    for j in range(len(layer_shapes[i])):
+        if layer_shapes[i][j] is not None:
+            layer_sizes[i] *= layer_shapes[i][j] 
 toolbox = Toolbox(layer_sizes=layer_sizes, 
                   state_shape=agent.state.shape,
                   num_samples=max_samples)
 
+# Train and test agent for specified number of epochs
 print("Starting the training!")
 test_scores_mean = []
-
-# Train and test agent for specified number of epochs
+time_start = time()
 for epoch in range(epochs):
     print("\nEpoch %d\n-------" % (epoch + 1))
     train_episodes_finished = 0
@@ -126,20 +128,23 @@ for epoch in range(epochs):
     # Testing
     print("\nTesting...")
     agent.reset_history()
+    save_epoch = (epoch + 1 == epochs or (epoch + 1) % save_freq == 0)
     for test_episode in trange(test_episodes_per_epoch):
         agent.initialize_new_episode()
         while not game.is_episode_finished():
-            print("Game tick %d of max %d" % game.get_episode_time 
-                                             game.get_episode_timeout, 
-                                             end='\r')
+            print("Game tick %d of max %d" 
+                  % (game.get_episode_time() - game.get_episode_start_time(), 
+                     game.get_episode_timeout()), 
+                  end='\r')
             agent.make_best_action()
-            agent.track_action()
-            agent.track_position()
-            if len(layer_names) > 0:
-                output = agent.get_layer_output(layer_names)
-                toolbox.update_max_data(state=agent.state, 
-                                        position=agent.position_history[-1],
-                                        layer_values=output)
+            if save_epoch:
+                agent.track_action()
+                agent.track_position()
+                if len(layer_names) > 0:
+                    output = agent.get_layer_output(layer_names)
+                    toolbox.update_max_data(state=agent.state, 
+                                            position=agent.position_history[-1],
+                                            layer_values=output)
         agent.update_score_history()
     test_scores = agent.get_score_history()
     test_scores_mean.append(agent.get_score_history().mean())
@@ -149,24 +154,31 @@ for epoch in range(epochs):
     
     # Save network params after specified number of epochs; 
     # otherwise store temporarily after each epoch
-    if epoch + 1 == epochs or (epoch + 1) % save_freq == 0:
+    if save_epoch:
         results_file_path = results_directory + exp_name + "_model"
         print("Saving network weights in:", results_file_path)
         agent.save_model(results_file_path, global_step=epoch+1, 
                          save_meta=(epoch == 0))
         if trackable:
+            print("Saving tracking data in:", results_directory)
             np.savetxt(results_directory + "positions_epoch" + str(epoch+1) + ".txt",
                        agent.get_positions())
             np.savetxt(results_directory + "actions_epoch" + str(epoch+1) + ".txt",
                        agent.get_actions())
         if len(layer_names) > 0:
+            print("Saving layer data in:", results_directory)
             max_values, max_states, max_positions = toolbox.get_max_data()
-            for i in range(len(layer_names)):                    
-                np.save(results_directory + "max_values_" + str(epoch+1), 
+            for i in range(len(layer_names)):
+                slash = layer_names[i].find("/")
+                abbr_name = layer_names[i][0:slash]                    
+                np.save(results_directory + "max_values_%s-%d"
+                        % (abbr_name, epoch+1), 
                         max_values[i])
-                np.save(results_directory + "max_states_" + str(epoch+1),
+                np.save(results_directory + "max_states_%s-%d"
+                        % (abbr_name, epoch+1),
                         max_states[i])
-                np.save(results_directory + "max_positions_" + str(epoch+1),
+                np.save(results_directory + "max_positions_%s-%d"
+                        % (abbr_name, epoch+1),
                         max_positions[i])
     else:
         results_file_path = results_directory + exp_name + "_model"
