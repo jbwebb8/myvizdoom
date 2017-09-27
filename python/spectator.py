342d9c187569ba5d375341ac0aaebe2f1dab17c7
jbwebb <jbwebb@bcm.edu>
2017-09-27 13:13:33 -0500

    corrected arg parser bug

--- python/spectator.py
+++ python/spectator.py
@@ -19,6 +19,7 @@ def initialize_vizdoom(config_file_path):
     return game
 
 # Initialize DoomGame
+parser = argparse.ArgumentParser(description="Test a scenario.")
 parser.add_argument("config_file_path", 
                     help="config file for scenario")
 parser.add_argument("-t", "--test-episodes", type=int, default=10, metavar="",
